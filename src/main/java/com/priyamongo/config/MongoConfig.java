cb58c54364375146f1706ef92c8543c71eef3d4a
priyadutta <priyadutta@Ubuntu-22.04.myguest.virtualbox.org>
2023-03-20 14:42:03 +0000

    added compund key congigs

--- src/main/java/com/priyamongo/config/MongoConfig.java
+++ src/main/java/com/priyamongo/config/MongoConfig.java
@@ -3,15 +3,27 @@ package com.priyamongo.config;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.priyamongo.mongodemo.repository.TestRepo;
 
 @EnableMongoRepositories(basePackageClasses = TestRepo.class)
 @Configuration
-public class MongoConfig {
+public class MongoConfig extends AbstractMongoClientConfiguration {
     @Bean
     CommandLineRunner commandLineRunner(TestRepo testRepo) {
         return strings -> {};
+    }
+
+    @Override
+    public boolean autoIndexCreation() {
+        return true;
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        // TODO Auto-generated method stub
+        return "test-data";
     }
 }
--- src/main/java/com/priyamongo/mongodemo/model/TestModel.java
+++ src/main/java/com/priyamongo/mongodemo/model/TestModel.java
@@ -1,6 +1,7 @@
 package com.priyamongo.mongodemo.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
@@ -8,6 +9,11 @@ import lombok.Data;
 
 @Data
 @Document(collection = "test-data")
+@CompoundIndex(
+    name = "com-key", 
+    def = "{'taxableEntityId': 1, 'incomeSourceId': 1, 'version': 1}",
+    unique = true
+    )
 public class TestModel {
     @Id
     private String id;
