8495ded1f8e0f89c2a7b64aba056761ea0b2711d
priyadutta <priyadutta@Ubuntu-22.04.myguest.virtualbox.org>
2023-03-22 11:05:14 +0000

    remove unused import

--- src/main/java/com/priyamongo/mongodemo/repository/TestRepo.java
+++ src/main/java/com/priyamongo/mongodemo/repository/TestRepo.java
@@ -2,8 +2,6 @@ package com.priyamongo.mongodemo.repository;
 
 import com.priyamongo.mongodemo.model.TestModel;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface TestRepo extends MongoRepository<TestModel, String> {
